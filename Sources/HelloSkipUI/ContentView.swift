a2b702a15dea8954ecf11d2b1a5bd6101a3dc996
Abe White <abewhite@mac.com>
2025-02-06 13:11:29 -0600

    Exercise latest skip-fuse-ui updates

--- Sources/HelloSkipUI/ContentView.swift
+++ Sources/HelloSkipUI/ContentView.swift
@@ -7,22 +7,75 @@ import SwiftUI
 
 let logger = Logger(subsystem: "HelloSkipFuse", category: "ContentView")
 
-public struct ContentView: View {
+public struct ContentView : View {
+    @State var selectedTab: Tab = .first
+    @State var counter = Counter()
+    @State var count = 0
+
     public init() {
     }
 
-    // SKIP @nobridge
     public var body: some View {
-        VStack {
-            Text("Hello").foregroundStyle(.secondary)
-            Text("SkipFuseUI")
-            HStack {
-                Text("This is a:")
-                Button("Button!") {
-                    logger.error("TAP!")
+        TabView(selection: $selectedTab) {
+            NavigationStack {
+                VStack {
+                    Text("Hello").foregroundStyle(.secondary)
+                    Text("SkipFuseUI").foregroundStyle(.red).colorInvert()
+                    HStack {
+                        Text("Observable count: \(counter.count)")
+                        Button("Increment") {
+                            logger.error("TAP!")
+                            counter.count += 1
+                        }
+                    }
+                    HStack {
+                        Text("State count: \(count)")
+                        Button("Increment") {
+                            logger.error("TAP!")
+                            count += 1
+                        }
+                    }
+                    NavigationLink("Push binding") {
+                        BindingView(countBinding: $count)
+                    }
                 }
+                .border(.red, width: 3)
+            }
+            .tag(Tab.first)
+            .tabItem {
+                Text("FIRST")
+            }
+
+            Button("Go to FIRST") {
+                selectedTab = .first
+            }
+            .tag(Tab.second)
+            .tabItem {
+                Text("SECOND")
             }
         }
-        .border(.red, width: 3)
     }
+}
+
+enum Tab : Hashable {
+    case first
+    case second
+}
+
+struct BindingView : View {
+    @Binding var countBinding: Int
+
+    var body: some View {
+        HStack {
+            Text("Count binding: \(countBinding)")
+            Button("Increment") {
+                logger.error("TAP!")
+                countBinding += 1
+            }
+        }
+    }
+}
+
+@Observable class Counter {
+    var count = 0
 }
